c870405b852a49a6c56506b86cbefed5b12a6b66
wb_zhubochao <wb_zhubochao@meituan.com>
2021-05-18 17:29:13 +0800

    补全类型和for

--- packages/graph-core/src/behavior/base.ts
+++ packages/graph-core/src/behavior/base.ts
@@ -1,23 +1,33 @@
 import Graph from '../controller/graph'
-
+type Callback = (...args: any[]) => void
+interface GraphEvent { type: string, bindFunc: Callback }
 abstract class Base {
   graph: Graph
-  eventQueue = []
+  /**
+   * @todo 为什么事件队列不在拥有on函数的对象里？应该是on函数添加事件队列
+   */
+  eventQueue: GraphEvent[] = []
 
   constructor(graph: Graph) {
     this.graph = graph
   }
 
-  addEvent(type: string, callback: (...args: any[]) => void) {
-    const bindFunc = callback.bind(this)
+  /**
+   * 监听事件
+   * @param type 事件类型
+   * @param callback 事件回调
+   */
+  addEvent(type: string, callback: Callback) {
+    const bindFunc: Callback = callback.bind(this)
     this.eventQueue.push({ type, bindFunc })
     this.graph.on(type, bindFunc)
   }
 
   destory() {
-    this.eventQueue.forEach(item => {
-      this.graph.off(item.type, item.bindFunc)
-    })
+    for (let i = this.eventQueue.length - 1; i >= 0; i--) {
+      const event = this.eventQueue[i];
+      this.graph.off(event.type, event.bindFunc)
+    }
   }
 }
 

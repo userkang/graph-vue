20f0b7e21caf46f717d1559f01ff5a184eb8a4ae
zhangkangkang02 <zhangkangkang02@meituan.com>
2022-06-24 11:26:21 +0800

    fix: 修复布局后不resize group的问题

--- packages/graph-demo/src/views/NodeCell.vue
+++ packages/graph-demo/src/views/NodeCell.vue
@@ -3,7 +3,7 @@
     <GraphVue :data="dataMock" :action="action" @init="initGraph">
       <template #node="{ node }">
         <div v-if="node.model.type === 'group'" class="group-node">
-          <button v-if="node.model.collapsed" @click="showChildren(node)">
+          <button v-if="node.hasState('collapsed')" @click="showChildren(node)">
             展开
           </button>
           <button v-else @click="hideChildren(node)">隐藏</button>
@@ -61,14 +61,12 @@ const nodeCellMock = (): IDataModel => {
       {
         id: '4',
         label: 'parent',
-        type: 'group',
-        collapsed: true
+        type: 'group'
       },
       {
         id: '5',
         label: 'parent',
-        type: 'group',
-        collapsed: true
+        type: 'group'
       },
       {
         id: '6',
@@ -119,9 +117,9 @@ export default class NodeCell extends Vue {
     })
     node.update({
       width: 180,
-      height: 40,
-      collapsed: true
+      height: 40
     })
+    node.setState('collapsed')
     this.layout()
   }
 
@@ -130,9 +128,7 @@ export default class NodeCell extends Vue {
     children.forEach(child => {
       child.show()
     })
-    node.update({
-      collapsed: false
-    })
+    node.clearState('collapsed')
 
     this.resizeGroup(node)
     this.layout()
@@ -166,18 +162,16 @@ export default class NodeCell extends Vue {
       .getNodes()
       .filter(item => item.model.type === 'group')
     groups.forEach(group => {
-      if (group.model.collapsed) {
-        this.hideChildren(group)
-      } else {
-        this.showChildren(group)
-      }
+      this.hideChildren(group)
     })
   }
 
   layout() {
+    // 获取根节点
     const rootNodes = this.graph.getNodes().filter(item => !item.model.parentId)
     const edges: Record<string, IEdge> = {}
 
+    // 收集根节点相关的边
     rootNodes.forEach(item => {
       item.getEdges().forEach(edge => {
         if (!edges[edge.id]) {
@@ -186,32 +180,47 @@ export default class NodeCell extends Vue {
       })
     })
 
+    // 先对根节点进行布局
     this.graph.layout({
       data: { nodes: rootNodes, edges: Object.values(edges) }
     })
 
+    // 处理根节点下子节点布局
     rootNodes.forEach(item => {
       const childrenEdges: Record<string, IEdge> = {}
       const children = item.getChildren()
-      children.forEach(child => {
-        child.getEdges().forEach(edge => {
-          if (!childrenEdges[edge.id]) {
-            childrenEdges[edge.id] = edge
-          }
+
+      if (children.length) {
+        // 收集所有子节点的边
+        children.forEach(child => {
+          child.getEdges().forEach(edge => {
+            if (!childrenEdges[edge.id]) {
+              childrenEdges[edge.id] = edge
+            }
+          })
         })
-      })
 
-      const dagre = this.graph.layout({
-        data: { nodes: children, edges: Object.values(childrenEdges) }
-      })
+        // 对子节点布局。默认布局不能满足需求，需要获取实例自定义布局位置
+        const dagre = this.graph.layout({
+          data: { nodes: children, edges: Object.values(childrenEdges) }
+        })
 
-      dagre.nodes().forEach((v: string) => {
-        const childNode = this.graph.findNode(v) as INode
-        const { x, y } = dagre.node(v)
-        const posX = item.x + x - childNode.width / 2 + groupPadding
-        const posY = item.y + y - childNode.height / 2 + groupPadding
-        childNode.updatePosition(posX, posY)
-      })
+        // 通过布局实例返回的坐标点，自定义布局位置。
+        dagre.nodes().forEach((v: string) => {
+          const childNode = this.graph.findNode(v) as INode
+          const { x, y } = dagre.node(v)
+          const posX = item.x + x - childNode.width / 2 + groupPadding
+          const posY = item.y + y - childNode.height / 2 + groupPadding
+          childNode.updatePosition(posX, posY)
+        })
+      }
+    })
+
+    // 布局后，再更新下group节点的大小
+    rootNodes.forEach(item => {
+      if (item.getChildren().length && !item.hasState('collapsed')) {
+        this.resizeGroup(item)
+      }
     })
   }
 

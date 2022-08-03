f3a6d92d19dea0dd73c3d930814e7f511ca2c24d
wb_zhubochao <zbc18842606347@163.com>
2022-08-03 16:58:20 +0800

    删除translateBy

--- packages/graph-core/src/controller/graph.ts
+++ packages/graph-core/src/controller/graph.ts
@@ -241,10 +241,6 @@ export default class Graph extends EventEmitter {
     return this.viewController.translateBy(x, y)
   }
 
-  public translateBy(x: number, y: number) {
-    return this.viewController.translateBy(x, y)
-  }
-
   public getZoom() {
     return this.viewController.getZoom()
   }
@@ -283,13 +279,13 @@ export default class Graph extends EventEmitter {
     this.emit('datachange')
   }
 
-  public fitCenter() {
-    this.viewController.translateToCenter()
-  }
-
-  public center = (x: number, y: number) => {
-    const { width, height } = this.getSvgInfo()
-    this.translateTo(width - x, height - y)
+  public fitCenter(position?: { x: number; y: number }) {
+    if (position) {
+      const { width, height } = this.getSvgInfo()
+      this.translateTo(width - position.x, height - position.y)
+    } else {
+      this.viewController.translateToCenter()
+    }
   }
 
   public fullScreen(el?: HTMLElement) {

69a63e04747e136d6d731e11d9f91f5ba13b2f79
wb_zhubochao <zbc18842606347@163.com>
2022-09-30 15:49:46 +0800

    修改排序逻辑

--- packages/graph-core/src/item/useSortedItems.ts
+++ packages/graph-core/src/item/useSortedItems.ts
@@ -14,47 +14,63 @@ const getItemZindex = (item: Node | Edge) =>
 export const useSortedItems = () => {
   const list: Array<Node | Edge> = []
 
-  const onZIndexChange = (target: Item, key: string, prevValue: unknown) => {
-    if (!(target instanceof Node) && !(target instanceof Edge)) {
+  const moveItem = (item: Item) => {
+    if (!(item instanceof Node) && !(item instanceof Edge)) {
+      return
+    }
+    const prevIndex = list.indexOf(item)
+    const zIndex = getItemZindex(item)
+    if (list[prevIndex + 1] && zIndex >= getItemZindex(list[prevIndex + 1])) {
+      console.log('right')
+      // 向右移动
+      let i = prevIndex + 1
+      for (; i < list.length; i++) {
+        const rightZindex = getItemZindex(list[i])
+        if (zIndex < rightZindex) {
+          break
+        } else if (zIndex === rightZindex && item instanceof Edge) {
+          break
+        }
+      }
+      list.splice(i, 0, item)
+      list.splice(prevIndex, 1)
+    } else if (
+      list[prevIndex - 1] &&
+      zIndex <= getItemZindex(list[prevIndex - 1])
+    ) {
+      // 向左移动
+      console.log('left')
+      let i = prevIndex - 1
+      for (; i >= 0; i--) {
+        const leftZindex = getItemZindex(list[i])
+        if (zIndex > leftZindex) {
+          break
+        } else if (zIndex === leftZindex && item instanceof Node) {
+          break
+        }
+      }
+      list.splice(prevIndex, 1)
+      list.splice(i + 1, 0, item)
+    }
+  }
+
+  const onZIndexChange = (item: Item, key: string) => {
+    if (!(item instanceof Node) && !(item instanceof Edge)) {
       return
     }
     if (key !== 'zIndex') {
       return
     }
-    if (typeof prevValue !== 'number' && typeof prevValue !== 'undefined') {
-      return
-    }
-    const zIndex = getItemZindex(target)
+    moveItem(item)
   }
 
   const add = (item: Item) => {
-    if (item instanceof Node) {
-      const zIndex = item.zIndex
-      let i = list.length - 1
-      for (; i >= 0; i--) {
-        if (zIndex >= list[i].zIndex) {
-          break
-        }
-      }
-      list.splice(i + 1, 0, item)
-    } else if (item instanceof Edge) {
-      const zIndex = getItemZindex(item)
-      let i = list.length - 1
-      for (; i >= 0; i--) {
-        if (zIndex > list[i].zIndex) {
-          break
-        }
-      }
-      list.splice(i + 1, 0, item)
-    }
-    item.on('change', onZIndexChange)
-  }
-
-  const change = (item: Item) => {
     if (!(item instanceof Node) && !(item instanceof Edge)) {
       return
     }
-    const index = list.indexOf(item)
+    item instanceof Node ? list.push(item) : list.unshift(item)
+    moveItem(item)
+    item.on('change', onZIndexChange)
   }
 
   const remove = (item: Item) => {

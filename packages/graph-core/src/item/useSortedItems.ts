f79531a96f36803f5abf540b2a39672379640abd
wb_zhubochao <zbc18842606347@163.com>
2022-10-08 12:06:40 +0800

    insertItem

--- packages/graph-core/src/item/useSortedItems.ts
+++ packages/graph-core/src/item/useSortedItems.ts
@@ -7,7 +7,7 @@ const getItemZindex = (item?: Node | Edge) => (item ? item.zIndex || 0 : 0)
 export const useSortedItems = () => {
   const list: Array<Node | Edge> = []
 
-  const moveItem = (target: Node | Edge) => {
+  const insertItem = (target: Node | Edge) => {
     const getLocalZindex = (t: Node | Edge) =>
       getItemZindex(t) + (t instanceof Edge ? -0.5 : 0)
     const prevIndex = list.indexOf(target)
@@ -55,14 +55,14 @@ export const useSortedItems = () => {
     if (key !== 'zIndex') {
       return
     }
-    moveItem(item)
+    insertItem(item)
   }
 
   const add = (item: Item) => {
     if (!(item instanceof Node) && !(item instanceof Edge)) {
       return
     }
-    moveItem(item)
+    insertItem(item)
     item.on('change', onZIndexChange)
   }
 

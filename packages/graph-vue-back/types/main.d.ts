f6bdd79fe9dcb73379305d3a2bdd25c05b2a061e
zhangkangkang02 <zhangkangkang02@meituan.com>
2023-05-25 13:27:09 +0800

    删除graph-vue3目录

--- packages/graph-vue-back/types/main.d.ts
+++
@@ -1,8 +0,0 @@
-import GraphVue from '../components/graph.vue'
-import ToolBox from '../components/tool-box.vue'
-import Menu from '../components/menu.vue'
-import MiniMap from '../components/minimap.vue'
-
-export { GraphVue, ToolBox, Menu, MiniMap }
-export * from '@datafe/graph-core'
-export declare function install(vue: any): void

c8d624eb6ff5eb55563f15b3330de97e7f02c000
zhangkangkang02 <zhangkangkang02@meituan.com>
2023-06-06 20:47:18 +0800

    修改 eslint 配置

--- packages/graph-vue/.eslintrc.js
+++ packages/graph-vue/.eslintrc.js
@@ -4,13 +4,7 @@ module.exports = {
     browser: true,
     node: true
   },
-  extends: [
-    'plugin:vue/essential',
-    'eslint:recommended',
-    '@vue/typescript/recommended',
-    '@vue/prettier',
-    '@vue/prettier/@typescript-eslint'
-  ],
+  extends: ['plugin:vue/essential', 'eslint:recommended'],
   plugins: ['@typescript-eslint'],
   parserOptions: {
     parser: '@typescript-eslint/parser',

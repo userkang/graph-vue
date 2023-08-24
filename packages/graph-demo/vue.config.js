f0f0cf315db09ff8d6bef3718ed784cee828eeb6
zhangkangkang02 <zhangkangkang02@meituan.com>
2023-08-24 18:51:20 +0800

    fix: 修改配置文件

--- packages/graph-demo/vue.config.js
+++ packages/graph-demo/vue.config.js
@@ -7,6 +7,18 @@ const htmlPath = path.resolve(__dirname, './public/index.html')
 // Talos 线上构建会注入 PUBLIC_URL 环境变量
 const { PUBLIC_URL } = process.env
 
+const extraAlias =
+  process.env.NODE_ENV === 'development'
+    ? {
+        '@datafe/graph-core': path.resolve(__dirname, '../graph-core/src'),
+        '@datafe/graph-vue': path.resolve(__dirname, '../graph-vue/main.ts'),
+        '@datafe/vue-demi': path.resolve(
+          __dirname,
+          '../graph-vue/vue2/node_modules/@datafe/vue-demi'
+        )
+      }
+    : {}
+
 module.exports = {
   outputDir: path.resolve(__dirname, './build'),
   publicPath: process.env.NODE_ENV === 'production' ? PUBLIC_URL : './',
@@ -24,12 +36,7 @@ module.exports = {
       alias: {
         // https://github.com/LinusBorg/portal-vue/issues/201
         vue$: path.resolve(__dirname, 'node_modules/vue/dist/vue.esm.js'),
-        // '@datafe/graph-core': path.resolve(__dirname, '../graph-core/src'),
-        // '@datafe/graph-vue': path.resolve(__dirname, '../graph-vue/main.ts'),
-        // '@datafe/vue-demi': path.resolve(
-        //   __dirname,
-        //   '../graph-vue/vue2/node_modules/@datafe/vue-demi'
-        // )
+        ...extraAlias
       }
     },
     plugins: [
